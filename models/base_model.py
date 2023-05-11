ff55f4477a9b26ed5d94fc5dcb30df4687c60189
Hermelab1 <hermelabirhanu06@gmail.com>
2023-05-11 05:43:58 -0700

    Base model args

--- models/base_model.py
+++ models/base_model.py
@@ -4,13 +4,26 @@ import models
 import uuid 
 from datetime import datetime
 
+
 class BaseModel:
     """BaseModel class"""
-    def __init__(self):
-        """Initialize instance attributes"""
+
+    def __init__(self, *args,  **kwargs):
+        """
+        Initialize instance attributes
+        *kwargs a key value pairs of attributes
+        *args it is unused
+        """
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for i, j in kwargs.items():
+                if i == "created_at" or i == "updated_at":
+                    self.__dict__[i] = datetime.strptime(j, timeformat)
+                else:
+                    self.__dict__[i] = j
 
     def __str__(self):
         """Return string representation of BaseModel"""
@@ -18,12 +31,13 @@ class BaseModel:
 
     def save(self):
         """Update updated_at with current datetime"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """Return dictionary representation of BaseModel"""
         dic_copy = self.__dict__.copy()
-        dic_copy['__class__'] = self.__class__.__name__
         dic_copy['created_at'] = self.created_at.isoformat()
         dic_copy['updated_at'] = self.updated_at.isoformat()
+        dic_copy["__class__"] = self.__class__.__name__
         return dic_copy
