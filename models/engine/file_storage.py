eac9d43a461167d6f483361117569a8f848e49b1
nebihadilsebo <shegolenur@hotmail.com>
2023-05-13 03:08:06 -0700

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,21 +3,26 @@ import os.path
 
 
 class FileStorage:
+    """Serialization and deserialization to and from JSON file."""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Return the dictionary of objects."""
         return FileStorage.__objects
 
     def new(self, obj):
+        """Add a new object to the dictionary"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__object[key] = obj
 
     def save(self):
+        """Save the dictionary to the JSON file."""
         with open(FileStorage.__file_path, mode="w", encoding="utf-8")as file:
             json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items()}, file)
 
     def reload(self):
+        """Load the dictionary from the JSON file."""
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, mode="r", encoding="utf-8") as file:
                 objs = json.load(file)
