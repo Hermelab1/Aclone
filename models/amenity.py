d60a185b41a9dd4196288cfabe839c84a790e996
Hermelab1 <hermelabirhanu06@gmail.com>
2023-05-13 14:27:01 -0700

    classes

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """A amenity class have Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defination of state class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """A class state have Attributes name"""
+
+    name = ""
