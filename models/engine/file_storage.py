c01188c44d1d17d79b27f3b72037a65547ebda72
Hermelab1 <hermelabirhanu06@gmail.com>
2023-05-13 11:32:30 -0700

    fix on file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+"""A class to difine a File Storage class"""
 import json
 from models.base_model import BaseModel
 
 
 class FileStorage:
-    """A class to define file storage"""
+    """Defination of abstract storage"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Returns a dictionary objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets object with key class"""
@@ -28,8 +29,11 @@ class FileStorage:
         """to deserialize the json file to the object"""
         obdic = self.__objects
         try:
-            with open(self.__file_path) as f:
-                jload = json.load(f).items()
-                obdic = {k: BaseModel(**v) for k, v in jload}
+            with open(FileStorage.__file_path) as f:
+                obdict = json.load(f)
+                for o in obdict.values():
+                    class_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(class_name)(**o))
         except FileNotFoundError:
-            pass
+            return
